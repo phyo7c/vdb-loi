19cb4090496a5915618b139493fc06e8d40c36fc
7thcomputing <devuser@7thcomputing.com>
2022-11-11 16:55:58 +0630

    vdb-6 pathakha report

--- pathaka_py3o_report/models/report_pathaka.py
+++ pathaka_py3o_report/models/report_pathaka.py
@@ -13,9 +13,25 @@ class AccountMove(models.Model):
         for payslip_id in payslip_ids:
             m = payslip_id.date_from.month
             if m > 0 and m == month:
-                return payslip_id.salary
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'NET':
+                        return line_id.total
+                        break
             else:
-                return 0
+                return 0.0
+
+    def _get_payslip2(self,month):
+        payslip_ids = self.env['hr.payslip'].search([('id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == month:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'PIT':
+                        return line_id.total
+                        break
+            else:
+                return 0.0
+
 
     def _get_parents(self):
         f = m = 0
