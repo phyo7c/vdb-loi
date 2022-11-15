a38f11e8e8c3c462b3a9e122ce8a5afa0ed08250
7thcomputing <devuser@7thcomputing.com>
2022-11-15 10:23:19 +0630

    vdb-6::pathaka report

--- pathaka_py3o_report/models/report_pathaka.py
+++ pathaka_py3o_report/models/report_pathaka.py
@@ -15,6 +15,20 @@ class AccountMove(models.Model):
             if m > 0 and m == month:
                 for line_id in payslip_id.line_ids:
                     if line_id.code == 'NET':
+                        total = line_id.total
+                        total = f"{int(total):,}"
+                        return total
+                        break
+            else:
+                return 0.0
+
+    def _get_payslip_internal(self,month):
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == month:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'NET':
                         return line_id.total
                         break
             else:
@@ -27,10 +41,39 @@ class AccountMove(models.Model):
             if m > 0 and m == month:
                 for line_id in payslip_id.line_ids:
                     if line_id.code == 'PIT':
+                        total = line_id.total
+                        total = f"{int(total):,}"
+                        return total
+                        break
+            else:
+                return 0.0
+
+    def _get_payslip2_internal(self,month):
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == month:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'PIT':
                         return line_id.total
                         break
             else:
                 return 0.0
+
+
+    def _get_total(self):
+        total = 0
+        for x in range(1, 13):
+            total += self._get_payslip_internal(x)
+        total = f"{int(total):,}"
+        return total
+
+    def _get_total2(self):
+        total = 0
+        for x in range(1, 13):
+            total += self._get_payslip2_internal(x)
+        total = f"{int(total):,}"
+        return total
 
 
     def _get_parents(self):
