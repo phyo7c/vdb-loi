3b76ee52ef1ef1231b604ebf5e15880a17a8eef7
7thcomputing <devuser@7thcomputing.com>
2022-11-22 11:13:04 +0630

    15 nov

--- pathaka_py3o_report/models/report_pathaka.py
+++ pathaka_py3o_report/models/report_pathaka.py
@@ -9,13 +9,72 @@ class AccountMove(models.Model):
     _inherit = "hr.employee"
 
     def _get_payslip(self,month):
-        payslip_ids = self.env['hr.payslip'].search([('id','=',self.id)])
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
         for payslip_id in payslip_ids:
             m = payslip_id.date_from.month
             if m > 0 and m == month:
-                return payslip_id.salary
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'NET':
+                        total = line_id.total
+                        total = f"{int(total):,}"
+                        return total
+                        break
             else:
-                return 0
+                return 0.0
+
+    def _get_payslip_internal(self,month):
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == month:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'NET':
+                        return line_id.total
+                        break
+            else:
+                return 0.0
+
+    def _get_payslip2(self,month):
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == month:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'PIT':
+                        total = line_id.total
+                        total = f"{int(total):,}"
+                        return total
+                        break
+            else:
+                return 0.0
+
+    def _get_payslip2_internal(self,month):
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == month:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'PIT':
+                        return line_id.total
+                        break
+            else:
+                return 0.0
+
+
+    def _get_total(self):
+        total = 0
+        for x in range(1, 13):
+            total += self._get_payslip_internal(x)
+        total = f"{int(total):,}"
+        return total
+
+    def _get_total2(self):
+        total = 0
+        for x in range(1, 13):
+            total += self._get_payslip2_internal(x)
+        total = f"{int(total):,}"
+        return total
+
 
     def _get_parents(self):
         f = m = 0
