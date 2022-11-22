3d1b30f07536af98cf61c1dba7c744197cb9c0c2
7thcomputing <devuser@7thcomputing.com>
2022-11-22 11:18:50 +0630

    18 nov 1st commit

--- pathaka_py3o_report/models/report_pathaka.py
+++ pathaka_py3o_report/models/report_pathaka.py
@@ -104,3 +104,27 @@ class AccountMove(models.Model):
             m = 1
         return f+m
 
+    def _get_premium_fee(self):
+        return self.insurance_amt
+
+    def _get_ssb_fee(self):
+        return (12 * 6000)
+
+    def _get_basic_allowance(self):
+        payslip_ids = self.env['hr.payslip'].search([('employee_id','=',self.id)])
+        for payslip_id in payslip_ids:
+            m = payslip_id.date_from.month
+            if m > 0 and m == 5:
+                for line_id in payslip_id.line_ids:
+                    if line_id.code == 'PIT':
+                        total = round(line_id.total, 2)
+                        total = f"{total:,}"
+                        split_num = total.split(".")
+                        if len(split_num) >= 2:
+                            if len(split_num[1]) == 1:
+                                total = total + '0'
+                        return total
+                        break
+            else:
+                return 0.00
+
