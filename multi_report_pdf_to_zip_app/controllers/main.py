b6845a8dd38915740d6889dcc5235d6c5e54c58b
7thcomputing <devuser@7thcomputing.com>
2022-12-12 17:28:11 +0630

    VDBL-28: PaThaKha(WaNga) 15(A) report can't generate after install zip report

--- multi_report_pdf_to_zip_app/controllers/main.py
+++ multi_report_pdf_to_zip_app/controllers/main.py
@@ -9,7 +9,7 @@ import time
 import base64
 import zipfile
 import json
-
+import mimetypes
 from werkzeug.urls import url_decode
 
 
@@ -20,6 +20,42 @@ class ReportControllerInherited(ReportController):
 		'/report/<converter>/<reportname>/<docids>',
 	], type='http', auth='user', website=True)
 	def report_routes(self, reportname, docids=None, converter=None, **data):
+		if converter == "py3o":
+			context = dict(request.env.context)
+
+			if docids:
+				docids = [int(i) for i in docids.split(",")]
+			if data.get("options"):
+				data.update(json.loads(data.pop("options")))
+			if data.get("context"):
+				# Ignore 'lang' here, because the context in data is the
+				# one from the webclient *but* if the user explicitely wants to
+				# change the lang, this mechanism overwrites it.
+				data["context"] = json.loads(data["context"])
+				if data["context"].get("lang"):
+					del data["context"]["lang"]
+				context.update(data["context"])
+
+			ir_action = request.env["ir.actions.report"]
+			action_py3o_report = ir_action.get_from_report_name(
+				reportname, "py3o"
+			).with_context(context)
+			if not action_py3o_report:
+				raise exceptions.HTTPException(
+					description="Py3o action report not found for report_name "
+								"%s" % reportname
+				)
+			res, filetype = action_py3o_report._render(docids, data)
+			filename = action_py3o_report.gen_report_download_filename(docids, data)
+			if not filename.endswith(filetype):
+				filename = "{}.{}".format(filename, filetype)
+			content_type = mimetypes.guess_type("x." + filetype)[0]
+			http_headers = [
+				("Content-Type", content_type),
+				("Content-Length", len(res)),
+				("Content-Disposition", content_disposition(filename)),
+			]
+			return request.make_response(res, headers=http_headers)
 		report = request.env['ir.actions.report']._get_report_from_name(reportname)
 		context = dict(request.env.context)
 		if docids:
@@ -90,6 +126,31 @@ class ReportControllerInherited(ReportController):
 		"""
 		requestcontent = json.loads(data)
 		url, type = requestcontent[0], requestcontent[1]
+		if type == "py3o":
+			try:
+				reportname = url.split("/report/py3o/")[1].split("?")[0]
+				docids = None
+				if "/" in reportname:
+					reportname, docids = reportname.split("/")
+
+				if docids:
+					# Generic report:
+					response = self.report_routes(
+						reportname, docids=docids, converter="py3o"
+					)
+				else:
+					# Particular report:
+					# decoding the args represented in JSON
+					data = list(url_decode(url.split("?")[1]).items())
+					response = self.report_routes(
+						reportname, converter="py3o", **dict(data)
+					)
+				response.set_cookie("fileToken", token)
+				return response
+			except Exception as e:
+				se = _serialize_exception(e)
+				error = {"code": 200, "message": "Odoo Server Error", "data": se}
+				return request.make_response(html_escape(json.dumps(error)))
 		try:
 			if type in ['qweb-pdf', 'qweb-text']:
 				converter = 'pdf' if type == 'qweb-pdf' else 'text'
