ebb4059c71e269bf5ec0bfd856a3caddcf8cc5d1
7thcomputing <devuser@7thcomputing.com>
2022-12-16 16:41:12 +0630

    VDBL-30 and VDBL-31 (To add 'Report Date' in Income Tax Popup/ To change file name to (Name_ID_Individual Tax Calculation for month)

--- income_tax_report/wizards/hr_employee_tax_report.py
+++ income_tax_report/wizards/hr_employee_tax_report.py
@@ -1,11 +1,19 @@
 import time
 from odoo import api, fields, models
+from datetime import datetime
+
 
 class EmployeeTaxReport(models.TransientModel):
     _name = 'hr.employee.tax.report'
 
     fiscal_year_id = fields.Many2one('account.fiscal.year', string='Fiscal Year')
     emp = fields.Many2many('hr.employee', 'tax_report_emp_rel', 'tax_report_id', 'emp_id', string='Employee(s)')
+    report_date = fields.Date('Report Date', default=lambda self: fields.Date.today())
+
+    def _get_report_base_filename(self, emp_name, date_string, sub_area):
+        self.ensure_one()
+        month = datetime.strptime(date_string, '%Y-%m-%d').strftime('%b %d')
+        return '%s_%s %s_%s' % (emp_name, 'Individual Tax Calculation for', month, sub_area)
 
     def print_report(self):
         self.ensure_one()
